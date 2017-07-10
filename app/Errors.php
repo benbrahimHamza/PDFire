1b3a10599e73e1881f1f8e23659c156e6272a46a
Hamza Benbrahim <benbrahim.hamza@gmail.com>
2017-07-10 21:23:43 +0100

    Added responses in all cases (OK and NOK) and Config file messages, added '.pdf' extension for further use with split, also improved a little over the overall quality of the code.

---
+++ app/Errors.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+
+class Errors
+{
+    const UPLOAD_FILE_INVALID_ERROR = 'There was a problem during the upload of the file.';
+    const UPLOAD_FILE_MISSING_ERROR = 'PDF file must be supplied.';
+    const SPLIT_MISSING_FOLDER_ERROR = 'Upload folder doesn\'t exist';
+    const SPLIT_MISSING_FILE_ERROR = 'File not found in the upload folder';
+
+}
--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Laravel\Lumen\Routing\Controller as BaseController;
 use Illuminate\Http\Request;
 use App\Config;
+use App\Errors;
 
 
 class UploadController extends BaseController
@@ -20,18 +21,21 @@ class UploadController extends BaseController
     {
         // Check for the pdf
         if (!($request->hasFile('pdf_file'))) {
-            return "PDF file must be supplied.";
-        } // Upload problem
-        else if (!$request->file('pdf_file')->isValid()) {
-            return "There was a problem during the upload of the file.";
+            return response(Errors::UPLOAD_FILE_MISSING_ERROR, '404');
+        } else {
+          $pdfFile = $request->file('pdf_file');
+        }
+
+        // Upload problem
+        if (!$pdfFile->isValid()) {
+            return response(Errors::UPLOAD_FILE_INVALID_ERROR, '406');
         }
 
         // Move the validated file to the folder
-        $request->file('pdf_file')->move(Config::UPLAOD_PATH,
-            $request->file('pdf_file')->getFilename());
+        $pdfFile->move(Config::UPLOAD_PATH,
+        $pdfFile->getFilename() . '.pdf');
 
         // Return the moved file name
-        return [Config::UPLAOD_PATH. $request->file('pdf_file')->getFilename(),
-            $request->file('pdf_file')->getClientOriginalName()];
+        return response()->json(['fileName' => $pdfFile->getFilename(), 'fileOriginalName' => $pdfFile->getClientOriginalName()], '201');
     }
 }
